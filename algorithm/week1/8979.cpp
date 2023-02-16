46cdb93c90c7ba5ef5bae9378f5ba9217e92bb5f
pparkjyy <jyp96117@naver.com>
2023-02-16 20:25:11 +0900

    Create 8979.cpp

---
+++ algorithm/week1/8979.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+struct Nation {
+	int n, a, b, c;
+};
+
+int N, K;
+vector<Nation> v;
+
+int main() {
+	cin >> N >> K;
+	int n, a, b, c;
+	int idx = 0;
+	for (int i = 0; i < N; i++) {
+		cin >> n >> a >> b >> c;
+		v.push_back({ n,a,b,c });
+		if (n == K) idx = i;
+	}
+
+	int ans = 0;
+	Nation now = v[idx];
+	for (int i = 0; i < N; i++) {
+		Nation next = v[i];
+		if (next.a > now.a) { ans++; continue; }
+		else if (next.a == now.a) {
+			if (next.b > now.b) { ans++; continue; }
+			else if (next.b == now.b) {
+				if (next.c > now.c) { ans++; continue; }
+			}
+		}
+	}
+
+	cout << ans + 1;
+}
